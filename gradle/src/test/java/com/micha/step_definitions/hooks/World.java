c10bc309644f8b6f6620875014ee6d9889f78b34
mjmdescalzo <47499725+mjmdescalzo@users.noreply.github.com>
2024-01-15 15:33:22 +1300

    added World

---
+++ gradle/src/test/java/com/micha/step_definitions/hooks/World.java
@@ -0,0 +1,12 @@
+package com.micha.step_definitions.hooks;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class World {
+
+    public WebDriver driver;
+    public Properties executionProperties;
+
+}
